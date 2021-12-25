90bae939815bfac5898f2cb3159de62de3d03e7b
m3tac1ph4r <ashutoshg547@gmail.com>
2021-12-26 00:17:20 +0530

    updated new files

--- DAY_1/next_permutation.cpp
+++ DAY_1/next_permutation.cpp
@@ -5,8 +5,17 @@ const int M=1e9+7;
     
 int main()
 {
-    vector<int> nums={1,2,3};
-    int n=nums.size();
-    sort(nums.begin(),nums.end());
+    vector<int> nums={1,3,2};
+    if(next_permutation(nums.begin(),nums.end()))
+    {
+        for (auto i : nums)
+            cout << i << " ";
+    }
+    else
+    {
+        sort(nums.begin(),nums.end());
+        for (auto i : nums)
+            cout << i << " ";
+    }
     return 0;
 }
