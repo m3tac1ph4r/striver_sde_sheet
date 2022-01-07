514901dc6ee743d23dd4d444fa79e29f5a0326b1
m3tac1ph4r <ashutoshg547@gmail.com>
2022-01-07 14:55:25 +0530

    DAY_4 longest_substring_without_char_repeat added

---
+++ DAY_4/longest_substring_without_char_repeat.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+    
+int lengthOfLongestSubstring(string s)
+{
+    unordered_set<char> st;
+    int l=0,max_len=0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(st.find(s[i])!=st.end())
+        {
+            while(l<i and st.find(s[i])!=st.end())
+            {
+                st.erase(s[l]);
+                l++;
+            }
+        }
+        st.insert(s[i]);
+        max_len=max(max_len,i-l+1);
+    }
+    return max_len;
+}
+int main()
+{   
+    string s;
+    cin>>s;
+    cout<<lengthOfLongestSubstring(s);
+    return 0;
+}
+
+/*
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+*/
