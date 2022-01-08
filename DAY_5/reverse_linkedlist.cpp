6230e42edfa43384b11826228a1755bd154bef80
m3tac1ph4r <ashutoshg547@gmail.com>
2022-01-08 16:34:26 +0530

    DAY_5 reverse_linkedlist.cpp added

---
+++ DAY_5/reverse_linkedlist.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *reverseList(ListNode *head) 
+{
+    ListNode* prev=NULL;
+    ListNode* curr=head;
+    ListNode* next_p;
+    while(curr!=NULL)
+    {
+        next_p=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next_p;
+    }
+    head=prev;
+    return head;
+}
+
+int main()
+{
+    ListNode* head=new ListNode();
+    insertTail(head,1);
+    insertTail(head, 2);
+    insertTail(head, 3);
+    insertTail(head, 4);
+    display(head);
+    ListNode* temp=reverseList(head);
+    display(temp);
+    return 0;
+}
