f9d78973fbba0a14f14cebac5dd10760f3727dec
m3tac1ph4r <ashutoshg547@gmail.com>
2022-01-11 22:30:33 +0530

    DAY_6 linked_life_cycle.cpp added

---
+++ DAY_6/linked_life_cycle.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+    
+class ListNode
+{
+    public:
+    int val;
+    ListNode* next;
+    ListNode(int data)
+    {
+        val=data;
+        next=NULL;
+    }
+};
+void insertTail(ListNode *&head, int val)
+{
+    ListNode *tmp = new ListNode(val);
+    if (head == NULL)
+        head = tmp;
+    else
+    {
+        ListNode *n = head;
+        while (n->next != NULL)
+        {
+            n = n->next;
+        }
+        n->next = tmp;
+    }
+}
+void display(ListNode *head)
+{
+    while (head != NULL)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+bool hasCycle(ListNode* head)
+{
+    ListNode* fast=head;
+    ListNode* slow=head;
+    while(fast!=NULL and fast->next!=NULL)
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast)
+            return true;
+    }
+    return false;
+}
+int main()
+{
+    
+    return 0;
+}
+
+/*
+Using Floyd's Algorithm and Tortoise-Hare Approach
+https://leetcode.com/problems/linked-list-cycle/
+*/
