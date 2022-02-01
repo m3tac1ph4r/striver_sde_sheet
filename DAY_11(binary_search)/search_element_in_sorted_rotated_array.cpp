32806d04011dc012b78ce92e62e97d1a60dc3ade
m3tac1ph4r <ashutoshg547@gmail.com>
2022-02-01 09:36:33 +0530

    DAY_11 search_element_in_rotated_sorted_array.cpp

---
+++ DAY_11(binary_search)/search_element_in_sorted_rotated_array.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+    
+int search(vector<int> &nums,int target)
+{
+    int low=0,high=nums.size()-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(nums[mid]==target)
+            return mid;
+        if(nums[low]<=nums[mid])     // check whtther the left array is sorted or not
+        {
+            if(target<=nums[mid] and target>=nums[low])
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        else
+        {
+            if (target >= nums[mid] and target <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+                high = mid - 1;
+        }
+    }
+    return -1;
+}    
+int main()
+{
+    int n,target;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++)
+        cin>>nums[i];
+    cin>>target;
+    cout<<search(nums,target);
+    return 0;
+}
+
+/*
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+Using Binary Search
+
+TC: O(logn)
+SC: O(1)
+
+*/
+
+/*
+Naive Appraoch:
+TC: O(n)
+SC: O(1)
+
+int search(vector<int>& nums, int target) {
+    int ans=-1;
+    for(int i=0;i<nums.size();i++)
+    {
+        if(target==nums[i])
+        {
+            ans=i;
+            break;
+        }
+    }
+    return ans;
+}
+*/
