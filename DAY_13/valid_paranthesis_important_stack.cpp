2a49336bef9baaf8944afeb41542d962ef440f92
m3tac1ph4r <ashutoshg547@gmail.com>
2022-02-20 17:37:34 +0530

    DAY_13 valid_paranthesis_important_stack.cpp added

---
+++ DAY_13/valid_paranthesis_important_stack.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+    
+bool isValid(string expression)
+{
+    stack<char> arr;
+    if (expression.length() == 1 or expression.length() == 0)
+        return false;
+    else
+    {
+        for (int i = 0; i < expression.length(); i++)
+        {
+            if (arr.empty())
+            {
+                arr.push(expression[i]);
+                continue;
+            }
+            if (expression[i] == '(' or expression[i] == '[' or expression[i] == '{')
+            {
+                arr.push(expression[i]);
+            }
+            else if (expression[i] == ')' or expression[i] == ']' or expression[i] == '}')
+            {
+                if (!arr.empty())
+                {
+                    if (expression[i] == ')' and arr.top() == '(')
+                        arr.pop();
+                    else if (expression[i] == '}' and arr.top() == '{')
+                        arr.pop();
+                    else if (expression[i] == ']' and arr.top() == '[')
+                        arr.pop();
+                    else
+                        return false;
+                }
+            }
+        }
+    }
+    if (arr.empty())
+        return true;
+    else
+        return false;
+}    
+int main()
+{
+    string expression;
+    cin>>expression;
+    isValid(expression) == true ? cout << "True"<< "\n": cout << "False"<< "\n" ;
+    return 0;
+}
