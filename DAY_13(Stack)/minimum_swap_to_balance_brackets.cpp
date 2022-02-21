54d4d6f0cfd916b49308ecf9fa4082be786430dd
m3tac1ph4r <ashutoshg547@gmail.com>
2022-02-21 23:07:05 +0530

    DAY_13 minimum_swap_to_balance_brackets.cpp added

---
+++ DAY_13(Stack)/minimum_swap_to_balance_brackets.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+
+int findMinimumCost(string str)
+{
+    stack<char> myStack;
+    if(str.length()%2!=0)
+        return -1;
+    for(int i=0;i<str.length();i++)
+    {
+        if(str[i]=='{')
+            myStack.push(str[i]);
+        else
+        {
+            if(!myStack.empty() and myStack.top()=='{')
+            {
+                myStack.pop();
+            }
+            else
+                myStack.push(str[i]);
+        }
+    }
+    int openCount=0,closeCount=0;
+
+    while(!myStack.empty())
+    {
+        if(myStack.top()=='{')
+            openCount++;
+        else
+            closeCount++;
+        myStack.pop();
+    }
+    return ((openCount+1)/2+(closeCount+1)/2);
+}
+int main()
+{
+    string s;
+    cin>>s;
+    cout << findMinimumCost(s); 
+    return 0;
+}
