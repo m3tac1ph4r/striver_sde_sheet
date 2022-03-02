86e55d5104b6b685667790693abf36fb593d299f
m3tac1ph4r <ashutoshg547@gmail.com>
2022-03-02 23:50:14 +0530

    DAY_14 largest_rectange_in_histrogram,cpp added

---
+++ DAY_14/important_questions/largest_rectangle_in_histrogram.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+
+int nextSmallerElement(int i,vector<int> heights)
+{
+
+}
+int prevSmallerElement(int i,vector<int> heights)
+{
+
+}
+int largestRectangleArea(vector<int> &heights)
+{
+    
+}
+int main()
+{
+    
+    return 0;
+}
