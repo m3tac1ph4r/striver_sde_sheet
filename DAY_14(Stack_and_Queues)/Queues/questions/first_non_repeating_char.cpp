458f4549535c7945b2ba95d0733659a2ac8ce9d1
m3tac1ph4r <ashutoshg547@gmail.com>
2022-03-12 10:39:36 +0530

    DAY_14 queue/first_non_repeating_char.cpp added

---
+++ DAY_14(Stack_and_Queues)/Queues/questions/first_non_repeating_char.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+
+string FirstNonRepeating(string s)
+{
+    string ans;
+    unordered_map<char,int> count;
+    queue<char> q;
+    for(int i=0;i<s.length();i++)
+    {
+        // charachter ko added to the queue and increased the count in map
+        q.push(s[i]);
+        count[s[i]]++;
+        
+        while(!q.empty())
+        {
+            // if the character at the front is repeating or not if yes then remove it
+            if(count[q.front()]>1)
+                q.pop();
+            else
+            {
+                //if the character is non repeating then push it to the ans 
+                ans.push_back(q.front());
+                break;
+            }
+        }
+        //if all elements are repeating till s[i] then put # to the ans
+        if(q.empty())
+            ans.push_back('#');
+    }
+    return ans;
+} 
+int main()
+{
+    string s;
+    cin>>s;
+    cout << FirstNonRepeating(s); 
+    return 0;
+}
