78da595ca3bffa73d6734fa7782613c2a6ed791a
m3tac1ph4r <ashutoshg547@gmail.com>
2022-03-27 17:09:39 +0530

    updated

--- DAY_14(Stack_and_Queues)/Queues/questions/first_non_repeating_char.cpp
+++ DAY_14(Stack_and_Queues)/Queues/questions/first_non_repeating_char.cpp
@@ -10,13 +10,13 @@ string FirstNonRepeating(string s)
     queue<char> q;
     for(int i=0;i<s.length();i++)
     {
-        // charachter ko added to the queue and increased the count in map
+        // charachter added to the queue and increased the count in map
         q.push(s[i]);
         count[s[i]]++;
         
         while(!q.empty())
         {
-            // if the character at the front is repeating or not if yes then remove it
+            // check if the character is at the front is repeating or not if yes then remove it
             if(count[q.front()]>1)
                 q.pop();
             else
@@ -39,3 +39,6 @@ int main()
     cout << FirstNonRepeating(s); 
     return 0;
 }
+/*
+https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1#
+*/
