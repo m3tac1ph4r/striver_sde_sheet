20293a6b8901ced0d1ce58cfad9f6c600d77d7ef
m3tac1ph4r <ashutoshg547@gmail.com>
2022-03-30 19:56:01 +0530

    updated

---
+++ DAY_17(Binary_Tree)/important_questions/flatten_tree_into_linkedlist.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M = 1e9 + 7;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node *buildTree(Node *root)
+{
+    int data;
+    cout << "Enter Data : "
+         << "\n";
+    cin >> data;
+    root = new Node(data);
+    if (data == -1)
+        return NULL;
+
+    cout << "Enter data for inserting left of " << data << "\n";
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting right of " << data << "\n";
+    root->right = buildTree(root->right);
+    return root;
+}
+void flatten(Node *root)
+{
+    Node *current = root;
+    while (current != NULL)
+    {
+        if (current->left != NULL)
+        {
+            Node *prev = current->left;
+            while (prev != NULL)
+                prev = prev->right;
+            prev->right = current->right;
+            current->right = current->left;
+            current->left = NULL;
+        }
+        current = current->right;
+    }
+}
+int main()
+{
+    Node *root;
+    root = buildTree(root);
+
+    return 0;
+}
+
+/*
+Solved this using Morris Preorder traversal because we do not have to use any extra auxillary space.
+
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+https://practice.geeksforgeeks.org/problems/flatten-binary-tree-to-linked-list/1/#
+*/
