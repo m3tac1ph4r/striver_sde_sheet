2f2b78479b25e6841f0a930d3c6e8319396e530b
Ashutosh Gupta <Ashutoshg547@gmail.com>
2022-07-26 22:44:26 +0530

    DAY_9 subset_sum.cpp added

--- DAY_9(Recursion)/subset_sums.cpp
+++ DAY_9(Recursion)/subset_sums.cpp
@@ -1,38 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int M=1e9+7;
+const int M = 1e9 + 7;
 
-void solve(int i,vector<int> arr,int n,int sum,vector<int> &ans)
+void solve(int i, vector<int> arr, int n, int sum, vector<int> &ans)
 {
-    if(i==n)
+    if (i == n)
     {
         ans.push_back(sum);
         return;
     }
     else
     {
-        solve(i+1,arr,n,sum+arr[i],ans);    //element added to sum
-        solve(i+1, arr, n, sum, ans);      //element not added to sum
+        solve(i + 1, arr, n, sum + arr[i], ans); // element added to sum
+        solve(i + 1, arr, n, sum, ans);          // element not added to sum
     }
 }
-vector<int> subsetSums(vector<int> arr,int n)
+vector<int> subsetSums(vector<int> arr, int n)
 {
     vector<int> ans;
-    solve(0,arr,n,0,ans);
+    solve(0, arr, n, 0, ans);
     return ans;
 }
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> arr(n);
     for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];  
+        cin >> arr[i];
     }
-    vector<int> ans=subsetSums(arr,n);
-    for(auto i:ans)
-        cout<<i<<" ";
+    vector<int> ans = subsetSums(arr, n);
+    for (auto i : ans)
+        cout << i << " ";
     return 0;
 }
+
+// https://www.codingninjas.com/codestudio/problems/subset-sum_3843086
