c81128799846ae5e0ae74a71a88576ba4790189e
Ashutosh Gupta <Ashutoshg547@gmail.com>
2022-09-16 18:26:09 +0530

    UPDATED MERGE_TWO_SORTED_LIST.cpp

--- DAY_5(Linked_list)/merge_two_sorted_list.cpp
+++ DAY_5(Linked_list)/merge_two_sorted_list.cpp
@@ -1,7 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int M=1e9+7;
+const int M = 1e9 + 7;
 
 struct ListNode
 {
@@ -33,67 +33,65 @@ void display(ListNode *head)
         cout << head->val << " ";
         head = head->next;
     }
-    cout<<"\n";
+    cout << "\n";
 }
 
-ListNode* mergeTwoLists(ListNode* list1,ListNode* list2)
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
 {
-    ListNode* res=new ListNode(-1);
-    ListNode* p1=list1;
-    ListNode* p2=list2;
-    ListNode* p3=res;
-    while(p1!=NULL and p2!=NULL)
+    ListNode *res = new ListNode(-1);
+    ListNode *p1 = list1;
+    ListNode *p2 = list2;
+    ListNode *p3 = res;
+    while (p1 != NULL and p2 != NULL)
     {
-        if(p1->val<=p2->val)
+        if (p1->val <= p2->val)
         {
-            p3->next=p1;
-            p1=p1->next;
+            p3->next = p1;
+            p1 = p1->next;
         }
         else
         {
-            p3->next=p2;
-            p2=p2->next;
+            p3->next = p2;
+            p2 = p2->next;
         }
-        p3=p3->next;
+        p3 = p3->next;
     }
-    while(p1!=NULL)
+    if (p1 != NULL)
     {
-        p3->next=p1;
-        p1=p1->next;
+        p3->next = p1;
     }
-    while (p2 != NULL)
+    if (p2 != NULL)
     {
         p3->next = p2;
-        p2 = p2->next;
     }
     return res->next;
 }
 
-ListNode* mergeRecursive(ListNode* list1,ListNode* list2)
+ListNode *mergeRecursive(ListNode *list1, ListNode *list2)
 {
-    if(list1==NULL)
+    if (list1 == NULL)
         return list2;
-    if (list2==NULL)
+    if (list2 == NULL)
         return list1;
-    ListNode* res;
-    if(list1->val<list2->val)
+    ListNode *res;
+    if (list1->val < list2->val)
     {
-        res=list1;
-        res->next=mergeRecursive(list1->next,list2);
+        res = list1;
+        res->next = mergeRecursive(list1->next, list2);
     }
     else
     {
-        res=list2;
-        res->next=mergeRecursive(list1,list2->next);
+        res = list2;
+        res->next = mergeRecursive(list1, list2->next);
     }
     return res;
 }
 
 int main()
 {
-    ListNode* l1=new ListNode();
-    ListNode* l2=new ListNode();
-    insertTail(l1,-9);
+    ListNode *l1 = new ListNode();
+    ListNode *l2 = new ListNode();
+    insertTail(l1, -9);
     insertTail(l1, 3);
     insertTail(l2, 5);
     insertTail(l2, 7);
